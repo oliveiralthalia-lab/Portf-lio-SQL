f6893d9d55efac3d9b60a7296a115679f35a86dd
oliveiralthalia-lab <oliveiralthalia@gmail.com>
2026-09-18 11:20:08 -0300

    Criar dados-clientes.sql

---
+++ dados-clientes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE clientes (
+    id INT,
+    nome VARCHAR(100),
+    cidade VARCHAR(100)
+);
+
+INSERT INTO clientes (id, nome, cidade) VALUES
+(1, 'Ana', 'Belo Horizonte'),
+(2, 'Lucas', 'Contagem'),
+(3, 'Mariana', 'Belo Horizonte'),
+(4, 'João', 'Betim'),
+(5, 'Camila', 'Nova Lima');
